98ce3248115184a59019f91941a2cb0533eaa00e
hamsky13 <ilham.ius13@gmail.com>
2021-10-25 20:08:56 +0700

    BE menambahkan controller untuk user dapat melihat ongoing dan past transaction

--- routers/transactionRouter.js
+++ routers/transactionRouter.js
@@ -22,11 +22,16 @@ routers.get("/get-revenue-date/:par", transactionController.getRevenueDate);
 routers.get("/get-revenue", transactionController.getRevenue);
 
 routers.get("/get", transactionController.getTrx);
+routers.get(
+  "/get-usertransactions/:id",
+  transactionController.getUserTransaction
+);
 routers.get("/get-status", transactionController.getTrxStatus);
 routers.get("/get-trxproducts", transactionController.getTrxProductsStock);
 routers.get("/get-trxmanage/:par", transactionController.getTrxManage);
 routers.get("/get-trxmanageall/:par", transactionController.getTrxManageAll);
 routers.patch("/confirm-transaction/:id", transactionController.confirmTrx);
 routers.patch("/reject-transaction/:id", transactionController.rejectTrx);
+routers.patch("/parcel-diterima/:id", transactionController.updateOngoing);
 
 module.exports = routers;
